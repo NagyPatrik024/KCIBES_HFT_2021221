2dda1dd986a7996b5638fcc991934a1098c563a5
Patrik <Patrik@DESKTOP-V8A0R0V>
2021-10-07 15:53:46 +0200

    Add project files.

---
+++ KCIBES_HFT_2021221.Logic/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace KCIBES_HFT_2021221.Logic
+{
+    public class Class1
+    {
+    }
+}
---
+++ KCIBES_HFT_2021221.Models/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace KCIBES_HFT_2021221.Models
+{
+    public class Class1
+    {
+    }
+}
