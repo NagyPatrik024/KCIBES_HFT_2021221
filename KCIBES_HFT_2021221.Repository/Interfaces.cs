75d6a2b14d49890ed2604e7eefe7ac142ef88ae8
NagyPatrik <NagyPatrik024@stud.uni-obuda.hu>
2021-11-09 19:23:20 +0100

    Repository: interfaces

---
+++ KCIBES_HFT_2021221.Repository/Interfaces.cs
@@ -0,0 +1,34 @@
+﻿using KCIBES_HFT_2021221.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KCIBES_HFT_2021221.Repository
+{
+    public interface IRepository<T> where T : class
+    {
+        T GetOne(int id);
+        IQueryable<T> GetAll();
+
+        void UpdateOne(int id, T item);
+
+        void DeleteOne(int id);
+
+        void CreateOne(T item);
+    }
+
+    public interface IDriverRepository : IRepository<Driver>
+    {
+        void ChangeTeam(int id, Team team);
+    }
+    public interface IMotorRepository : IRepository<Motor>
+    {
+        void ChangeType(int id, string motortype);
+    }
+    public interface ITeamRepository : IRepository<Team>
+    {
+        void ChangeTeamChief(int id, string chiefname);
+    }
+}
