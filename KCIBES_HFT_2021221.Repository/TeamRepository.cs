dd658cc0639068fa50b6c87a82bd23605887f7df
NagyPatrik <NagyPatrik024@stud.uni-obuda.hu>
2021-11-09 20:02:31 +0100

    TeamRepository

--- KCIBES_HFT_2021221.Repository/TeamRepository.cs
+++ KCIBES_HFT_2021221.Repository/TeamRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using KCIBES_HFT_2021221.Data;
+using KCIBES_HFT_2021221.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,75 @@ using System.Threading.Tasks;
 
 namespace KCIBES_HFT_2021221.Repository
 {
-    class TeamRepository
+    public class TeamRepository : Repository<Team>, ITeamRepository
     {
+
+        public TeamRepository(F1DbContext ctx) : base(ctx)
+        {
+
+        }
+        public void ChangeTeamChief(int id, string chiefname)
+        {
+            Team team = _ctx.Teams.FirstOrDefault<Team>(x => x.TeamId == id);
+            if (team != null)
+            {
+                team.Team_Chief = chiefname;
+                _ctx.SaveChanges();
+            }
+            else
+            {
+                throw new Exception(); // TODO 
+            }
+        }
+
+        public override void CreateOne(Team team)
+        {
+            _ctx.Teams.Add(team);
+            _ctx.SaveChanges();
+        }
+
+        public override void DeleteOne(int id)
+        {
+            Team team = _ctx.Teams.FirstOrDefault<Team>(x => x.TeamId == id);
+            if (team != null)
+            {
+                _ctx.Teams.Remove(team);
+                _ctx.SaveChanges();
+            }
+            else
+            {
+                throw new Exception(); // TODO 
+            }
+
+        }
+
+        public override Team GetOne(int id)
+        {
+            Team team = _ctx.Teams.FirstOrDefault<Team>(x => x.TeamId == id);
+            if (team != null)
+            {
+                return team;
+            }
+            else
+            {
+                throw new Exception(); // TODO 
+            }
+        }
+
+        public override void UpdateOne(int id, Team item)
+        {
+            Team team = _ctx.Teams.FirstOrDefault<Team>(x => x.TeamId == id);
+            if (team != null)
+            {
+                _ctx.Teams.Remove(team);
+                _ctx.SaveChanges();
+            }
+            else
+            {
+                throw new Exception(); // TODO 
+            }
+        }
+
+
     }
 }
